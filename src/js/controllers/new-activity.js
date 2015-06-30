9cc494ddd53619cbd9b8a85046c57ad29d21c89b
John Joseph <jmj1444@gmail.com>
2015-06-29 21:48:34 -0400

    added submit new but post isnt working

--- src/js/controllers/new-activity.js
+++ src/js/controllers/new-activity.js
@@ -7,7 +7,28 @@ var router = require('../router');
 var show = require('../show');
 
 router.route('activities/new', function () {
-	show('new-activity');
+	show('new-template');
+
+	$('.new-record-form').on('submit', function (e) {
+		e.preventDefault();
+
+		var activity = $('.activity-field').val();
+		var description = $('.description-field').val();
+
+		var newActivity = {
+			'activity_name': activity,
+			'description': description
+		};
+
+	  $.ajax({
+		  method: "GET",
+		  url: "/api/activities/",
+		  data: { newActivity }
+		})
+	  	.done(function( msg ) {
+	    	alert( "Data Saved: " + msg );
+  	});
 
 
+	})
 });
