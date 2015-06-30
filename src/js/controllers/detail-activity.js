b84004171aca5d6452a1fa0f1731d1cf17e04a5a
John Joseph <jmj1444@gmail.com>
2015-06-30 14:21:23 -0400

    added delete function

--- src/js/controllers/detail-activity.js
+++ src/js/controllers/detail-activity.js
@@ -5,8 +5,11 @@ var _ = require('underscore');
 var views = require('views');
 var router = require('../router');
 var show = require('../show');
+var getCookie = require('../csrf.js');
 
 router.route('activities/:id', function (id) {
+
+	var csrftoken = getCookie('csrftoken');	
 
 	$.ajax({
 	  method: "GET",
@@ -15,6 +18,24 @@ router.route('activities/:id', function (id) {
 	  .done(function(data) {
 	    show('detail-template', { activity: data });
 	});
+
+
+
+
+	$('.main-content').on('click', '.rmv-btn' , function() {
+		console.log('hello')
+
+		$.ajax({
+			beforeSend: function (request)
+            {
+             request.setRequestHeader('X-CSRFToken', csrftoken);
+            },
+			method: "DELETE",
+			url: "/api/activities/" + id 
+		}).done(function() {
+			console.log('great')
+		})
+	})  
 
 
 
