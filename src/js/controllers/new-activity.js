106759468e7ef442222decf68e3d11e034f6e447
John Joseph <jmj1444@gmail.com>
2015-06-29 22:17:13 -0400

    added some ajax calls

--- src/js/controllers/new-activity.js
+++ src/js/controllers/new-activity.js
@@ -6,29 +6,29 @@ var views = require('views');
 var router = require('../router');
 var show = require('../show');
 
-router.route('activities/new', function () {
-	show('new-template');
+// router.route('activities/new', function () {
+// 	show('new-template');
 
-	$('.new-record-form').on('submit', function (e) {
-		e.preventDefault();
+// 	$('.new-record-form').on('submit', function (e) {
+// 		e.preventDefault();
 
-		var activity = $('.activity-field').val();
-		var description = $('.description-field').val();
+// 		var activity = $('.activity-field').val();
+// 		var description = $('.description-field').val();
 
-		var newActivity = {
-			'activity_name': activity,
-			'description': description
-		};
+// 		var newActivity = {
+// 			'activity_name': activity,
+// 			'description': description
+// 		};
 
-	  $.ajax({
-		  method: "GET",
-		  url: "/api/activities/",
-		  data: { newActivity }
-		})
-	  	.done(function( msg ) {
-	    	alert( "Data Saved: " + msg );
-  	});
+// 	  $.ajax({
+// 		  method: "GET",
+// 		  url: "/api/activities/",
+// 		  data: { newActivity }
+// 		})
+// 	  	.done(function( msg ) {
+// 	    	alert( "Data Saved: " + msg );
+//   	});
 
 
-	})
-});
+// 	})
+// });
--- stattracker/static/js/app.js
+++ stattracker/static/js/app.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";var $=require("jquery"),_=require("underscore"),views=require("views"),router=require("../router"),show=require("../show");router.route("activities/:id/details",function(e){show("detail-template")});
+"use strict";var $=require("jquery"),_=require("underscore"),views=require("views"),router=require("../router"),show=require("../show");router.route("activities/:id",function(e){show("detail-template"),$.ajax({method:"GET",url:"/api/activities/"+e}).done(function(e){show("detail-template",{activity:e}),console.log(e)})});
 
 },{"../router":6,"../show":7,"jquery":"jquery","underscore":"underscore","views":"views"}],2:[function(require,module,exports){
 "use strict";var $=require("jquery"),_=require("underscore"),views=require("views"),router=require("../router"),show=require("../show");router.route("activities/:id/edit",function(e){show("edit-activity")});
@@ -8,7 +8,7 @@
 "use strict";var $=require("jquery"),_=require("underscore"),views=require("views"),router=require("../router"),show=require("../show");router.route("","activities",function(){$.ajax({method:"GET",url:"/api/activities/"}).done(function(e){show("home",{activities:e})})});
 
 },{"../router":6,"../show":7,"jquery":"jquery","underscore":"underscore","views":"views"}],4:[function(require,module,exports){
-"use strict";var $=require("jquery"),_=require("underscore"),views=require("views"),router=require("../router"),show=require("../show");router.route("activities/new",function(){show("new-template"),$(".new-record-form").on("submit",function(e){e.preventDefault();$(".activity-field").val(),$(".description-field").val();$.ajax({method:"GET",url:"/api/activities/"}).done(function(e){console.log(e)})})});
+"use strict";var $=require("jquery"),_=require("underscore"),views=require("views"),router=require("../router"),show=require("../show");
 
 },{"../router":6,"../show":7,"jquery":"jquery","underscore":"underscore","views":"views"}],5:[function(require,module,exports){
 "use strict";var router=require("./router");({controllers:{"detail-activity":require("./controllers/detail-activity.js"),"edit-activity":require("./controllers/edit-activity.js"),"home-activities":require("./controllers/home-activities.js"),"new-activity":require("./controllers/new-activity.js")}}),router.init();
