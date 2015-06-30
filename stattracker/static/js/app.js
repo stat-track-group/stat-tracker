f6e5d8eb6b6e534681548e9e7861c1ad046ec067
John Joseph <jmj1444@gmail.com>
2015-06-29 21:27:10 -0400

    a

--- stattracker/static/js/app.js
+++ stattracker/static/js/app.js
@@ -1,15 +1,11 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-<<<<<<< HEAD
-"use strict";var $=require("jquery"),_=require("underscore"),views=require("views"),router=require("../router"),show=require("../show");router.route("",function(){show("home"),$.ajax({method:"GET",url:"/api/activities/"}).done(function(e){console.log(e)})});
-=======
 "use strict";var $=require("jquery"),_=require("underscore"),views=require("views"),router=require("../router"),show=require("../show");router.route("activities/:id/details",function(e){show("detail-template")});
 
 },{"../router":6,"../show":7,"jquery":"jquery","underscore":"underscore","views":"views"}],2:[function(require,module,exports){
 "use strict";var $=require("jquery"),_=require("underscore"),views=require("views"),router=require("../router"),show=require("../show");router.route("activities/:id/edit",function(e){show("edit-activity")});
 
 },{"../router":6,"../show":7,"jquery":"jquery","underscore":"underscore","views":"views"}],3:[function(require,module,exports){
-"use strict";var $=require("jquery"),_=require("underscore"),views=require("views"),router=require("../router"),show=require("../show");router.route("",function(){show("home")});
->>>>>>> 330d6c814a44496cccc46c6abf683f9980407845
+"use strict";var $=require("jquery"),_=require("underscore"),views=require("views"),router=require("../router"),show=require("../show");router.route("",function(){show("home"),$.ajax({method:"GET",url:"/api/activities/"}).done(function(e){console.log(e)})});
 
 },{"../router":6,"../show":7,"jquery":"jquery","underscore":"underscore","views":"views"}],4:[function(require,module,exports){
 "use strict";var $=require("jquery"),_=require("underscore"),views=require("views"),router=require("../router"),show=require("../show");router.route("activities/new",function(){show("new-activity")});
